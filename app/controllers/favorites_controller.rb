fe09a7ad4eaf59eeee4ee5173cf3dd6ac54ad533
Ubuntu <ubuntu@ip-172-31-43-161.ap-northeast-1.compute.internal>
2026-08-08 10:29:57 +0000

    Add favorite and comment featutes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,12 @@
 class FavoritesController < ApplicationController
   def create
-    @favorite = Current.user.favorites.build(book_id: params[:book_id])
-    @favorite.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @favorite = Current.user.favorites.find_or_create_by(book: @book)
   end
+
 
   def destroy
     @favorite = Current.user.favorites.find(params[:id])
     @favorite.destroy
-    redirect_to request.referer
   end
 end
